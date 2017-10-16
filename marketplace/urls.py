92f509e4ff9cee62203c21452874a39e985cf4a8
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-15 23:48:31 -0700

    Add URL patterns

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from marketplaceapp import views
@@ -6,4 +6,7 @@ from marketplaceapp import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_details, name='gig_details'),
+    url(r'^social/', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^auth/', include('django.contrib.auth.urls', namespace='auth')),
 ]
---
+++ marketplaceapp/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from marketplaceapp import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_details, name='gig_details'),
+    url('', include('marketplaceapp.urls'))
+]
