22a0cd5aa03fba8e61d868e923aae8a4141c04c4
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-17 11:42:17 -0700

    Typo, constant name

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-AUTHENTIFICATION_BACKENDS = (
+AUTHENTICATION_BACKENDS = (
     'social.backends.facebook.FacebookOAuth2',
     'django.contrib.auth.backends.ModelBackend'
 )
