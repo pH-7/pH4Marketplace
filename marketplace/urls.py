41b0a83522d7d8c3aa1a635505898eaea04709ae
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-19 21:05:24 -0700

    Use url function to call the homepage URL

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from marketplaceapp import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^admin/', admin.site.urls),
     url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_details, name='gig_details'),
     url(r'^social/', include('social_django.urls', namespace='social')),
     url(r'^auth/', include('django.contrib.auth.urls', namespace='auth')),
