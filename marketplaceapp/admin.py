7dcabe324cbf6119859137b75a101942ba544eea
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-21 20:47:41 -0700

    Add first model + add it into admin panel

--- marketplaceapp/admin.py
+++ marketplaceapp/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from marketplaceapp.models import Profile
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Profile)
--- marketplaceapp/models.py
+++ marketplaceapp/models.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.contrib.auth.models import User
 
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.CharField(max_length=500)
+    about = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.user.username
