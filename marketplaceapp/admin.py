afaa2577eaadae107fd7361cd2c8129442bc398e
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-22 11:43:16 -0700

    Add gig model

--- marketplaceapp/admin.py
+++ marketplaceapp/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from marketplaceapp.models import Profile
+from marketplaceapp.models import Profile, Gig
 from django.contrib import admin
 
 admin.site.register(Profile)
+admin.site.register(Gig)
--- marketplaceapp/models.py
+++ marketplaceapp/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from django.db import models
 
@@ -12,3 +13,24 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Gig(models.Model):
+    CATEGORY_CHOICES = (
+        ("SS", "Scripts & Software"),
+        ("GD", "Graphics & Design"),
+        ("WM", "Web Marketing"),
+        ("V", "Videos"),
+        ("M", "Music")
+    )
+
+    title = models.CharField(max_length=500)
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
+    description = models.CharField(max_length=1000)
+    price = models.IntegerField(default=6)
+    photo = models.FileField(upload_to='gigs')
+    status = models.BooleanField(default=True)
+    user = models.ForeignKey(User)
+    create_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
