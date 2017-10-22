8fe10f2a49619ac8aecdfdf914f08f01f40c0503
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-22 11:42:45 -0700

    Add MEDIA path + ignore media gigs folder

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -111,9 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # To serve files to Heroku
 
-# To serve static files to Heroku
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Replace DB config to use PostgreSQL for Heroku
--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from marketplaceapp import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
-    url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_details, name='gig_details'),
     url(r'^social/', include('social_django.urls', namespace='social')),
     url(r'^auth/', include('django.contrib.auth.urls', namespace='auth')),
-]
+    url('', include('marketplaceapp.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
