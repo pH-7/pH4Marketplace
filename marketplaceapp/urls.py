367298e89fe45810d8df7c13ff70fba8461bbb7e
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-22 22:23:34 -0700

    Add create_gig/my_gigs views + urls

--- marketplaceapp/urls.py
+++ marketplaceapp/urls.py
@@ -4,4 +4,6 @@ from marketplaceapp import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_details, name='gig_details'),
+    url(r'^my_gigs/$', views.my_gigs, name='my_gigs'),
+    url(r'^create_gig/$', views.create_gig, name='create_gig'),
 ]
--- marketplaceapp/views.py
+++ marketplaceapp/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from marketplace.settings import MEDIA_URL
 from marketplaceapp.models import Gig
+from marketplaceapp.forms import GigForm
 
 def home(request):
     gigs = Gig.objects.filter(status=True)
@@ -13,6 +15,32 @@ def gig_details(request, id):
     try:
         gig = Gig.objects.get(id=id)
     except Gig.DoesNotExist:
-        return redirect('/')
+        return redirect(home)
 
     return render(request, 'gig_details.html', {'gig': gig, 'media_url': MEDIA_URL})
+
+@login_required(login_url='/')
+def create_gig(request):
+    error = '' # Default msg value
+    if request.method == 'POST':
+        gig_form = GigForm(request.POST, request.FILES)
+        if gig_form.is_valid():
+            gig = gig_form.save(commit=False)
+            gig.user = request.user
+            gig.save()
+            return redirect(my_gigs)
+        else:
+            error = 'The data is not valid'
+
+    gig_form = GigForm()
+
+    return render(
+        request, 'create_gig.html',
+        {'gig_form': gig_form, 'error': error}
+    )
+
+@login_required(login_url='/')
+def my_gigs(request):
+    gigs = Gig.objects.filter(user=request.user)
+
+    return render(request, 'my_gigs.html', {'gigs': gigs})
